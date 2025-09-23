47a5b48aca334c19629a03c65c3c49e78feb9ac2
Asura <kiba741@gmail.com>
2025-09-23 14:45:43 +0000

    feat: add Sanity Studio integration and StartupCard component
    - Updated pnpm workspace to include esbuild as a built dependency.
    - Created a new page for Sanity Studio at app/studio/[[...tool]]/page.tsx.
    - Added StartupCard component for displaying startup posts with relevant details.
    - Configured Sanity CLI and environment variables for project and dataset.
    - Defined Sanity configuration and schema structure for content management.
    - Implemented live content fetching capabilities with Sanity.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 import SearchForm from "../../components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+
+export interface post {
+  _createdAt: Date;
+  views: number;
+  author: { _id: number; name: string };
+  _id: number;
+  description: string;
+  image: string;
+  category: string;
+  title: string;
+}
 
 const Home = async ({
   searchParams,
@@ -7,6 +19,20 @@ const Home = async ({
   searchParams: Promise<{ query?: string }>;
 }) => {
   const { query } = await searchParams;
+
+  const posts: post[] = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Micheal Scott" },
+      _id: 1,
+      description: "This is a description",
+      image:
+        "https://images.unsplash.com/photo-1559136555-9303baea8ebd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+      category: "Office",
+      title: "The Office",
+    },
+  ];
 
   return (
     <>
@@ -26,6 +52,13 @@ const Home = async ({
         <p className=" text-3xl font-medium">
           {query ? `Search results for ${query}` : "All StartUps"}
         </p>
+        <div className="mt-7 grid grid-cols-2 sm:grid-cols-3  gap-5">
+          {posts.length > 0 ? (
+            posts.map((post) => <StartupCard key={post._id} post={post} />)
+          ) : (
+            <p>No startups found</p>
+          )}
+        </div>
       </section>
     </>
   );
---
+++ components/StartupCard.tsx
@@ -0,0 +1,80 @@
+import { post } from "@/app/(root)/page";
+import { formatDate } from "@/lib/utils";
+import { EyeIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+
+const StartupCard = ({ post }: { post: post }) => {
+  const {
+    _createdAt,
+    _id,
+    author: { _id: authorId, name },
+    views,
+    category,
+    description,
+    image,
+    title,
+  } = post;
+  return (
+    <div className="bg-white border-[5px] border-black py-6 px-5 rounded-[22px] w-full shadow-200 hover:border-primary transition-all duration-500 hover:shadow-300 hover:bg-primary-100 group">
+      <div className="flex items-center justify-between">
+        <p className="font-medium text-[16px] bg-primary-100 px-4 py-2 rounded-full group-hover:bg-white-100">
+          {formatDate(_createdAt)}
+        </p>
+        <div className="flex gap-1 5">
+          <EyeIcon className="size-6 text-primary" />
+          <span className="text-medium">{views}</span>
+        </div>
+      </div>
+      <div className="flex justify-between items-center mt-5 gap-5">
+        <div className="flex-1">
+          <Link href={`/user/${authorId}`}>
+            <p className="font-medium text-[16px] text-black line-clamp-1">
+              {name}
+            </p>
+          </Link>
+          <Link href={`/startup/${_id}`}>
+            <h3 className="font-semibold text-[26px] text-black line-clamp-1">
+              {title}
+            </h3>
+          </Link>
+        </div>
+        <Link href={`/user/${authorId}`}>
+          <Image
+            src="https://placehold.co/48x48"
+            unoptimized
+            alt={description}
+            width={48}
+            height={48}
+            className="rounded-full"
+          />
+        </Link>
+      </div>
+      <Link href={`/startup/${_id}`}>
+        <p className="font-normal text-[16px] line-clamp-2 my-3 text-black-100 break-all">
+          {description}
+        </p>
+        <Image
+          src={image}
+          alt={description}
+          className="w-full h-[164px] rounded-[10px] object-cover"
+          unoptimized
+          width={82}
+          height={164}
+        />
+      </Link>
+      <div className="flex items-center justify-between gap-3 mt-5">
+        <Link href={`/?query=${category.toLowerCase()}`}>
+          <p className="font-medium text-[16px] text-black">{category}</p>
+        </Link>
+        <Button className="rounded-full bg-black-200 font-medium text-[16px] text-white px-5 py-3" asChild>
+          <Link href={`/startup/${_id}`}>Details</Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default StartupCard;
